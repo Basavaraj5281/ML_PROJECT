fdffd7761e868196c2071cad45ec5c4dd1bda6fa
Basavaraj <Basavarajmalipatil5281@gmail.com>
2025-11-21 16:25:25 +0530

    Created Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,46 @@
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+import pandas as pd 
 
+@dataclass
+class Dataingestionconfig:
+    train_data_path: str=os.path.join('artifacts',"Train.csv")
+    test_data_path: str=os.path.join('artifacts',"Test.csv")
+    raw_data_path: str=os.path.join('artifacts',"Data.csv")
+ 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=Dataingestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion method ")
+        try:
+            logging.info("started reading the data from source")
+            df=pd.read_csv("data\stud.csv")
+            logging.info("Read data into dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Starting splitting data into train and test ")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=True,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=True,header=True)
+            logging.info("Files Saved In Respective Folder")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
